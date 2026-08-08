ee087e23019307e65f56ef51d2421080fbeb9356
Fukuto <nekotonaito2@gmailcom>
2026-08-08 14:46:17 +0900

    タイマーを停止する機能を追加

--- UnityProject/Assets/FukutoScripts/Process/CountDownTime.cs
+++ UnityProject/Assets/FukutoScripts/Process/CountDownTime.cs
@@ -5,17 +5,21 @@ public class CountDownTime : CountBase
 {
     [SerializeField] private GameObject textStartCount; // テキストスタートカウント
     [SerializeField] private ManageCountDonw manageCountDown;
+    private bool isStop = false;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        // 時間の設定
+      // 時間の設定
         currentTime = settingTime;
     }
 
     // Update is called once per frame
     void Update()
     {
+        // タイマーを停止
+        if (isStop) return;
+
         if (!isApplyCount) return;
 
         CountDown();
@@ -28,5 +32,11 @@ public class CountDownTime : CountBase
     {
         int time = (int)Mathf.Ceil(currentTime);
         manageCountDown.SetCountDonw(time);
+    }
+
+    // タイマーを停止する関数
+    public void StopCount()
+    {
+        isStop = true;
     }
 }
