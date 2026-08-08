f1578e1b7fcfe5eb2bcfeeb86ce370951b5c17e7
Fukuto <nekotonaito2@gmailcom>
2026-08-08 14:56:39 +0900

    resultscriptの追加

---
+++ UnityProject/Assets/FukutoScripts/Process/ResultScript.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class ResultScript : MonoBehaviour
+{
+    [SerializeField] private GameManager gameManager;
+    //void Update()
+    //{
+    //    if(gameManager.)
+    //}
+}
